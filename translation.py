ccecfebf43db174140f430c2e237001d897104c9
DARKEMPIRE <95665347+DARKEMPIRESL@users.noreply.github.com>
2022-06-06 20:27:59 +0530

    `Auto-Forward-Bot`

--- translation.py
+++ translation.py
@@ -1,13 +1,13 @@
 class Translation(object):
 
       # Start text
-      START = "Hi I am a channel auto forward bot Named 𝗖𝗛𝗔𝗡𝗡𝗘𝗟 𝗔𝗨𝗧𝗢𝗠𝗔𝗧𝗘 𝗕𝗢𝗧</a>. \nMake your own bot now 👉 Source code \n\n<b>Support Channel and Groups:</b>\n @Ns_bot_updates, @Ns_Bot_supporters"             
+      START = "Hi I am a channel auto forward bot Named 𝗖𝗛𝗔𝗡𝗡𝗘𝗟 𝗔𝗨𝗧𝗢𝗠𝗔𝗧𝗘 𝗕𝗢𝗧</a>. \nMake your own bot now 👉 Source code \n\n<b>Support Channel and Groups:</b>\n @SLBotsChat, @SLBotOfficial"             
 
       #About text
       ABOUT = """
 <b>📝 Language:</b> Python 3
 <b>🧰 Framework:</b> Pyrogram
 <b>👨‍💻 Developer:</b> 𝕯𝖆𝖗𝖐 𝕰𝖒𝖕𝖎𝖗𝖊
-<b>📢 Channel:</b> NS BOT UPDATES
+<b>📢 Channel:</b> Team SL Bots🇱🇰
 <b>👥 Group:</b> Team SL Bots Chat🇱🇰
 <b>🌐Source Code:</b> Press Me 🥰"""
